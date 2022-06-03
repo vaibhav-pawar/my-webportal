819980c00ab46fb59d400c40d7e9718f87b511f2
Vaibhav Pawar <vaibhavpawar924@gmail.com>
2022-06-03 18:06:20 +0530

    kk

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -51,14 +51,14 @@ def get_segment(request):
 @blueprint.route('/logs/')
 @login_required
 def logs():
-    filenames = os.listdir('logs/AvailabiltyReports/DailyReports/')
+    filenames = os.listdir('apps/logs/AvailabiltyReports/DailyReports/')
     return render_template('downloadavailabilityreport.html', files=filenames)
 
 @blueprint.route('/<path:filename>')
 @login_required
 def log(filename):
     return send_from_directory(
-        os.path.abspath('logs/AvailabiltyReports/DailyReports/'),
+        os.path.abspath('apps/logs/AvailabiltyReports/DailyReports/'),
         filename,
         as_attachment=True
     )
